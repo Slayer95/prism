97a12f27cb18f457f61c8cdd391ae867c71f69e4
Ivo Julca <ivojulca@hotmail.com>
2026-06-09 22:43:02 -0500

    Add api/parse.js

---
+++ api/parse.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const chalk = require('chalk');
+
+const JASSParser = require('./../src/parser/parser');
+
+const cliOptions = {
+	spec: {
+		type: 'string',
+		multiple: false,
+		short: 'S',
+		default: 'jass2',
+	},
+	w3version: {
+		type: 'string',
+		multiple: false,
+		short: 'w',
+		default: '1.24',
+	},
+	quiet: {
+		type: 'boolean',
+		multiple: false,
+		short: 'q',
+		default: false,
+	},
+};
+
+const cliConfig = {
+	options: cliOptions,
+	strict: true,
+	allowPositionals: true,
+	tokens: false,
+};
+
+function main() {
+	const {values, positionals} = util.parseArgs(cliConfig);
+	const {error, trees} = JASSParser.parseFiles(positionals);
+	for (const [filePath, tree] of trees) {
+		if (values.quiet) continue;
+		console.log(`# Tree for ${filePath}`);
+		console.log(tree.rootNode.toString());
+		console.log(``);
+	}
+
+	if (error) {
+		process.exitCode = 1;
+	}
+
+	if (!values.quiet) {
+		if (error) {
+			console.error(chalk.red(`not ok`));
+		} else {
+			console.log(chalk.green(`ok`));
+		}
+	}
+}
+
+main();
