dcaba5f50078e5c9436df78652289865fb81d47b
Ivo Julca <ivojulca@hotmail.com>
2026-06-11 15:16:58 -0500

    Expand predicate logic

--- src/logic/predicate.js
+++ src/logic/predicate.js
@@ -13,33 +13,9 @@ const Quantifier = {
 
 	kSomeTimes: QuantifierBasis.kSome,
 	kAlways: QuantifierBasis.kSome | QuantifierBasis.kAll,
+
+	kIndeterminate: 0b10,
 };
-
-function mergePartitionQuantifiers(quantifiers) {
-	let any = false;
-	let all = true;
-
-	for (const op of quantifiers) {
-		any ||= (op !== Quantifier.kNone);
-		all &&= (op === Quantifier.kAll);
-	}
-
-	if (all) {
-		return Quantifier.kNone;
-	}
-	if (any) {
-		return Quantifier.kSome;
-	}
-	return Quantifier.kAll;
-}
-
-function isPartitionAny(quantifiers) {
-	return mergePartitionQuantifiers(quantifiers) === Quantifier.kSome;
-}
-
-function isPartitionAll(quantifiers) {
-	return mergePartitionQuantifiers(quantifiers) === Quantifier.kAll;
-}
 
 function isNever(quantifier) {
 	return quantifier === 0;
@@ -58,8 +34,230 @@ function toString(quantifier) {
 		case Quantifier.kNone: return 'none';
 		case Quantifier.kSome: return 'some';
 		case Quantifier.kAll: return 'all';
+		case Quantifier.kIndeterminate: return 'indeterminate';
 		default: return 'invalid';
 	}
+}
+
+function countQuantifiers(predicates) {
+	let none = 0;
+	let some = 0;
+	let all = 0;
+	let indeterminate = 0;
+
+	for (const q of predicates) {
+		switch (q) {
+			case Quantifier.kNone: ++none; break;
+			case Quantifier.kSome: ++some; break;
+			case Quantifier.kAll: ++all; break;
+			case Quantifier.kIndeterminate: ++indeterminate; break;
+		}
+	}
+
+	return { none, some, all, indeterminate };
+}
+
+// Given a predicate P, and a universe set U, split in a
+// partition p1, ..., pi, such that the extent of the predicates
+// over each partition is Q1, ..., Qi
+//
+// Take the extent Q1, ..., Qi,
+// and return extent Q' for universe set.
+function strictMergePartitionQuantifiers(quantifiers) {
+	let any = false;
+	let all = true;
+
+	for (const op of quantifiers) {
+		any ||= (op !== Quantifier.kNone);
+		all &&= (op === Quantifier.kAll);
+		if (op === Quantifier.kIndeterminate) {
+			return Quantifier.kIndeterminate;
+		}
+	}
+
+	if (all) {
+		return Quantifier.kAll;
+	}
+	if (any) {
+		return Quantifier.kSome;
+	}
+	return Quantifier.kNone;
+}
+
+const mergePartitionQuantifiers = {
+	strict: strictMergePartitionQuantifiers,
+	lower(quantifiers) {
+		let any = false;
+		let all = true;
+
+		for (const op of quantifiers) {
+			any ||= (op !== Quantifier.kNone && op !== Quantifier.kIndeterminate);
+			all &&= (op === Quantifier.kAll);
+		}
+
+		if (all) {
+			return Quantifier.kAll;
+		}
+		if (any) {
+			return Quantifier.kSome;
+		}
+		return Quantifier.kNone;
+	},
+	upper(quantifiers) {
+		let any = false;
+		let all = true;
+
+		for (const op of quantifiers) {
+			any ||= (op !== Quantifier.kNone);
+			all &&= (op === Quantifier.kAll || op === Quantifier.kIndeterminate);
+		}
+
+		if (all) {
+			return Quantifier.kAll;
+		}
+		if (any) {
+			return Quantifier.kSome;
+		}
+		return Quantifier.kNone;
+	},
+};
+
+function orderReversingInvolution(quantifier) {
+	// Returns a new quantifier Q', such that
+	// Q(P) -> Q'(not P)
+	switch (quantifier) {
+		case Quantifier.kNone: return Quantifier.kAll;
+		case Quantifier.kSome: return Quantifier.kSome;
+		case Quantifier.kAll: return Quantifier.kNone;
+		default: return quantifier;
+	}
+}
+
+const predicateExtentOperators = {
+	not: orderReversingInvolution,
+	strict: {
+		not: orderReversingInvolution,
+		and(predicates) {
+			const { none, some, indeterminate } = countQuantifiers(predicates);
+
+			if (none > 0) {
+				return Quantifier.kNone;
+			}
+			if (indeterminate > 0) {
+				return Quantifier.kIndeterminate;
+			}
+			if (some === 0) {
+				return Quantifier.kAll;
+			}
+			if (some === 1) {
+				return Quantifier.kSome;
+			}
+			return Quantifier.kIndeterminate;
+		},
+
+		or(predicates) {
+			const { some, all, indeterminate } = countQuantifiers(predicates);
+
+			if (all > 0) {
+				return Quantifier.kAll;
+			}
+			if (indeterminate > 0) {
+				return Quantifier.kIndeterminate;
+			}
+			if (some === 0) {
+				return Quantifier.kNone;
+			}
+			if (some === 1) {
+				return Quantifier.kSome;
+			}
+			return Quantifier.kIndeterminate;
+		},
+	},
+	bounded: {
+		lower: {
+			not(predicate) {
+				// Indeterminate treated as None
+				if (predicate === Quantifier.kIndeterminate) {
+					return Quantifier.kNone;
+				}
+				return orderReversingInvolution(predicate);
+			},
+			and(predicates) {
+				// Indeterminate treated as None
+				const { none, some, indeterminate } = countQuantifiers(predicates);
+
+				if (none + indeterminate > 0) {
+					return Quantifier.kNone;
+				}
+				if (some === 0) {
+					return Quantifier.kAll;
+				}
+				if (some === 1) {
+					return Quantifier.kSome;
+				}
+				return Quantifier.kNone;
+			},
+
+			or(predicates) {
+				// Indeterminate treated as None
+				const { some, all } = countQuantifiers(predicates);
+
+				if (all > 0) {
+					return Quantifier.kAll;
+				}
+				if (some === 0) {
+					return Quantifier.kNone;
+				}
+				return Quantifier.kSome;
+			},
+		},
+
+		upper: {
+			not(predicate) {
+				// Indeterminate treated as All
+				if (predicate === Quantifier.kIndeterminate) {
+					return Quantifier.kAll;
+				}
+				return orderReversingInvolution(predicate);
+			},
+			and(predicates) {
+				// Indeterminate treated as All
+				const { none, some } = countQuantifiers(predicates);
+
+				if (none > 0) {
+					return Quantifier.kNone;
+				}
+				if (some === 0) {
+					return Quantifier.kAll;
+				}
+				return Quantifier.kSome;
+			},
+
+			or(predicates) {
+				// Indeterminate treated as All
+				const { some, all, indeterminate } = countQuantifiers(predicates);
+
+				if (all + indeterminate > 0) {
+					return Quantifier.kAll;
+				}
+				if (some === 0) {
+					return Quantifier.kNone;
+				}
+				if (some === 1) {
+					return Quantifier.kSome;
+				}
+				return Quantifier.kAll;
+			},
+		},
+	},
+};
+
+function isPartitionAny(quantifiers) {
+	return isSomeTimes(mergePartitionQuantifiers.lower(quantifiers));
+}
+
+function isPartitionAll(quantifiers) {
+	return isAlways(mergePartitionQuantifiers.lower(quantifiers));
 }
 
 module.exports = {
@@ -67,8 +265,12 @@ module.exports = {
 	Quantifier,
 
 	toString,
+
 	isNever, isSomeTimes, isAlways,
 	mergePartitionQuantifiers,
+
+	orderReversingInvolution,
+	predicateExtentOperators,
 
 	isPartitionAny,
 	isPartitionExists: isPartitionAny,
