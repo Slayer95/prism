5ab43fb50f74ac2c3c1b89a289be4fcddd8775c3
Ivo Julca <ivojulca@hotmail.com>
2026-06-13 14:54:40 -0500

    Fix file encoding in string fixture

---
+++ test/gen-chartable.js
@@ -0,0 +1,28 @@
+function genCharTable() {
+	const output = [];
+	const isEscapable = charCode => {
+		return charCode === 8 || charCode === 9 || charCode === 0x0A || charCode === 0x0D;
+	};
+
+	const buffers = [];
+	const header = `function InitCharTable takes nothing returns nothing\n`;
+	const footer = `endfunction\n`;
+
+	buffers.push(Buffer.from(header));
+
+	// 0x00 and 0xFF are not allowed.
+	for (let i = 1; i < 0xFF; i++) {
+		const c = String.fromCharCode(i);
+		if (c !== '"' && c !== '\\') {
+			buffers.push(Buffer.from(`\tset charTable[${i}] = "${c}"\n`, 'latin1'));
+		}
+		if (isEscapable(i)) {
+			buffers.push(Buffer.from(`\tset charTable[${i}] = ${JSON.stringify(c)}\n`, 'latin1'));
+		}
+	}
+
+	buffers.push(Buffer.from(footer));
+	return Buffers.concat(buffers);
+}
+
+//process.stdout.write(genCharTable());
